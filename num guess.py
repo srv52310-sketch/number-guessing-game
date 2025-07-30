9766053f1c2e28bd2faa9542be76fa81525ae11c
srv52310-sketch <srv52310@gmail.com>
2025-07-30 08:53:42 +0530

    Add files via upload

---
+++ num guess.py
@@ -0,0 +1,13 @@
+import random
+winning_num = random.randint(1,100)
+guess = int(input("Guess a number between 1 and 100: "))
+while guess != winning_num:
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    if guess > winning_num:
+        print("Try lower!")
+    elif guess < winning_num:
+        print("Try higher!")
+    elif guess == winning_num:
+        print("You guessed right!")
+        break
